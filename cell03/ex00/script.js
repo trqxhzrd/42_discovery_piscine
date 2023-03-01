13e5facd0191da3765df9a4e341ff5b08373b82b
trqxhzrd <trqyhardpro@gmail.com>
2023-03-01 22:17:44 +0100

    added files

--- cell03/ex00/script.js
+++ cell03/ex00/script.js
@@ -1,11 +1,7 @@
-window.addEventListener("DOMContentLoaded", function() {
-    let btn = this.document.getElementById("btn");
-
-    btn.addEventListener("click", function() {
-        document.body.style.background = randColor();      
-    });
-});
-
 const randColor = () =>  {
     return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
 };
+
+function changecouleur() {
+    $("body").css("background-color", randColor ());
+}
