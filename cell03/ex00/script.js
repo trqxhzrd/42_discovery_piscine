14ed6d6930484264a57a5bb680bae68a8c456001
trqxhzrd <trqyhardpro@gmail.com>
2023-03-02 22:45:57 +0100

    update files

--- cell03/ex00/script.js
+++
@@ -1,7 +0,0 @@
-const randColor = () =>  {
-    return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
-};
-
-function changecouleur() {
-    $("body").css("background-color", randColor ());
-}
